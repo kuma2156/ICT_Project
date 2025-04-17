fa62c349c3eb28cb095a2b45eb4d73bbf6d7a42d
ddingyu <116462554+kuma2156@users.noreply.github.com>
2025-04-17 12:08:09 +0900

    Add files via upload

---
+++ market-tracker-reactive/src/main/java/com/example/markettrackerreactive/MarketTrackerReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.markettrackerreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTrackerReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketTrackerReactiveApplication.class, args);
+	}
+
+}
---
+++ market-tracker-reactive/src/test/java/com/example/markettrackerreactive/MarketTrackerReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markettrackerreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketTrackerReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
